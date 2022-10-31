ea3017f281840f0eb310a2aebcf17b454c4a5bec
Ermi <manermidem@gmail.com>
2022-10-31 22:26:00 +0300

    Add time.js file in modules folder

---
+++ modules/time.js
@@ -0,0 +1,13 @@
+// timer
+const date = document.querySelector('.time');
+const { DateTime } = window.luxon;
+export const times = () => {
+  date.innerHTML = DateTime.now().toLocaleString({
+    weekday: 'short',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+};
