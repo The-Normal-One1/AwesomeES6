9a604158af7fd88c561d122d8ab9eb6fdacde325
Ermi <manermidem@gmail.com>
2022-11-01 11:02:19 +0300

    Add luxon file

--- modules/time.js
+++ modules/time.js
@@ -1,17 +1,8 @@
 // timer
+import { DateTime } from "./luxon.js";
 const displayDate = () => {
   const date = new Date();
-  const options = {
-    weekday: undefined,
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-  const [month, time] = [
-    date.toLocaleDateString(undefined, options),
-    date.toLocaleTimeString().toLocaleLowerCase(),
-  ];
-  document.querySelector('.time').innerHTML = `${month}, ${time}`;
+  document.querySelector('.time').innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 };
 
 export default displayDate;
