aacfdb5b7e2c8f50f22874647d6be349add5da95
Ermi <manermidem@gmail.com>
2022-11-01 11:07:13 +0300

    Resolve linters error

--- modules/time.js
+++ modules/time.js
@@ -1,7 +1,7 @@
 // timer
-import { DateTime } from "./luxon.js";
+import { DateTime } from './luxon.js';
+
 const displayDate = () => {
-  const date = new Date();
   document.querySelector('.time').innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 };
 
